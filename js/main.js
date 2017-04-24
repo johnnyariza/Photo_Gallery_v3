3b806f01c402ac0b624e242d60ba4212fa874420
JohnnyAriza <johnnyarizac@hotmail.com>
2017-04-24 06:08:58 -0400

    Ready for 1st submission.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
-$('#search').hideseek({
-    attribute: 'title'
-});
+function searching() {
+    var input = document.getElementById('search');
+    var filter = input.value.toUpperCase();
+    var ul = document.getElementById('photos');
+    var li = ul.getElementsByTagName('li');
+    var i;
+    var a;
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName('a')[0].getAttribute('data-title');
+        if (a.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+//$('#search').hideseek({
+//    attribute: 'title'
+//});
 
 $('.search').sticky({
     topSpacing: 8,
